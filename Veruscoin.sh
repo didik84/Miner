a0b40abad81fb2675f9e48270beb0a08907743f2
didik84 <54639335+didik84@users.noreply.github.com>
2024-07-05 23:57:08 +0700

    Create Veruscoin.sh

---
+++ Veruscoin.sh
@@ -0,0 +1,7 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RCmCZZ8pcfTo6cKootKLMH65txukKa8mkm.android -p x --cpu 1
